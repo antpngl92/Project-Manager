6da0b5e4fccfa602e7c42c144a78480ac8907861
Anton <nyagolov.anton@gmail.com>
2023-11-21 22:28:20 +0200

    Add mock data

---
+++ src/data.mock.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    title: "Learning React",
+    date: "Dec 29, 2024",
+    description: "Learn React from the group.",
+    tasks: [],
+    active: false,
+  },
+  {
+    id: 2,
+    title: "Mastering React",
+    date: "Dec 29, 2024",
+    description: "Learn React from the group.",
+    tasks: [],
+    active: false,
+  },
+];
