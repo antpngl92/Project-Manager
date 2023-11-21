b59263f5c9d031d8c32b8b2e98ebd5ae186407d9
Anton <nyagolov.anton@gmail.com>
2023-11-21 22:27:40 +0200

    Remove pages

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,6 @@ module.exports = {
   webpack: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
     },
   },
 };
